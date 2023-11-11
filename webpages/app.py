f7b70d441e12132f37971e4b6dc7e485a050fcdf
Lore <loredevelopment@gmail.com>
2023-11-11 12:31:08 -0700

    Update app.py to allow for wildcard interface selection

--- webpages/app.py
+++ webpages/app.py
@@ -31,4 +31,4 @@ def table():
     return render_template('static.html', recipes=recipesRaw)
 
 if __name__ == '__main__':\
-    app.run(debug=True, host='192.168.1.44', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
