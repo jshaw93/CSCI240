e95f88a1cfab7166a4da4ec019af264153d5e50b
jshaw93 <loredevelopment@gmail.com>
2023-11-13 06:19:59 +0000

    Fix app.py to have wildcard inferface

--- webpages/app.py
+++ webpages/app.py
@@ -46,4 +46,4 @@ def table():
     return render_template('static.html', recipes=recipes)
 
 if __name__ == '__main__':\
-    app.run(debug=True, host='192.168.1.44', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
